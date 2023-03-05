f39dabe87ca94273d1d8c5fab692a9b9ca9d8e9f
melvinchia3636 <melvinchia623600@gmail.com>
2023-03-05 23:11:27 +0800

    UI redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import {
   addDoc,
   collection,
+  deleteDoc,
   doc,
   getDoc,
+  getDocs,
   onSnapshot,
   setDoc,
   updateDoc,
 } from "@firebase/firestore";
 import { Icon } from "@iconify/react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import {
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router";
+import { Link } from "react-router-dom";
 import { v4 } from "uuid";
 import { firestore } from "./firebase";
 
@@ -21,21 +31,46 @@ const servers = {
   iceCandidatePoolSize: 10,
 };
 
-let pc = new RTCPeerConnection(servers);
-
-function App() {
+function CallScreen() {
+  const pc = useMemo(() => new RTCPeerConnection(servers), []);
   const [localStream, setLocalStream] = useState(null);
   const [remoteStream, setRemoteStream] = useState(null);
-  const [callInput, setCallInput] = useState("");
-  const [createCallButtonDisabled, setCreateCallButtonDisabled] =
-    useState(true);
-  const [answerCallButtonDisabled, setAnswerCallButtonDisabled] =
-    useState(true);
+  const [webCamButtonDisabled, setWebCamButtonDisabled] = useState(false);
   const [hangUpButtonDisabled, setHangUpButtonDisabled] = useState(true);
   const [isWebCamOn, setIsWebCamOn] = useState(false);
+  const [linkCopied, setLinkCopied] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { id } = useParams();
+  const { isNew } = location.state ?? {};
+
+  const [_callID, _setCallID] = useState("");
+  const callID = useRef(_callID);
+  const setCallID = (data) => {
+    callID.current = data;
+    _setCallID(data);
+  };
 
   const webcamVideo = useRef();
   const remoteVideo = useRef();
+
+  useEffect(() => {
+    startWebCam().then(() => {
+      if (!isNew) {
+        answerCall(id);
+        return;
+      }
+
+      createCall(id);
+    });
+    pc.oniceconnectionstatechange = (e) => {
+      if (
+        ["disconnected", "failed", "closed"].includes(pc.iceConnectionState)
+      ) {
+        hangupCall();
+      }
+    };
+  }, []);
 
   useEffect(() => {
     if (localStream) {
@@ -59,17 +94,6 @@ function App() {
     }
   }, [remoteStream]);
 
-  useEffect(() => {
-    pc.oniceconnectionstatechange = (e) => {
-      if (
-        ["disconnected", "failed", "closed"].includes(pc.iceConnectionState)
-      ) {
-        console.log("sus");
-        hangupCall();
-      }
-    };
-  }, []);
-
   const startWebCam = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
@@ -80,19 +104,16 @@ function App() {
     const rmStream = new MediaStream();
     setRemoteStream(rmStream);
 
-    setCreateCallButtonDisabled(false);
-    setAnswerCallButtonDisabled(false);
+    setWebCamButtonDisabled(true);
     setIsWebCamOn(true);
   };
 
-  const createCall = async () => {
-    const id = v4();
-
+  const createCall = async (id) => {
     const callDocRef = doc(firestore, "calls", id);
     const offerCandidates = collection(callDocRef, "offerCandidates");
     const answerCandidates = collection(callDocRef, "answerCandidates");
 
-    setCallInput(id);
+    setCallID(id);
 
     const offerDescription = await pc.createOffer();
     await pc.setLocalDescription(offerDescription);
@@ -105,6 +126,11 @@ function App() {
     await setDoc(callDocRef, { offer });
 
     onSnapshot(callDocRef, (snapshot) => {
+      if (!snapshot.exists()) {
+        navigate("/ended");
+        return;
+      }
+
       const data = snapshot.data();
       if (!pc.currentRemoteDescription && data?.answer) {
         const answerDescription = new RTCSessionDescription(data.answer);
@@ -128,9 +154,16 @@ function App() {
     setHangUpButtonDisabled(false);
   };
 
-  const answerCall = async () => {
-    const callId = callInput;
+  const answerCall = async (callId) => {
+    setCallID(callId);
     const callDocRef = doc(firestore, "calls", callId);
+
+    const callDoc = await getDoc(callDocRef);
+    if (!callDoc.exists()) {
+      navigate("/call-not-exist");
+      return;
+    }
+
     const answerCandidates = collection(callDocRef, "answerCandidates");
     const offerCandidates = collection(callDocRef, "offerCandidates");
 
@@ -141,7 +174,6 @@ function App() {
     const callData = (await getDoc(callDocRef)).data();
 
     const offerDescription = callData.offer;
-    console.log(offerDescription);
     await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
 
     const answerDescription = await pc.createAnswer();
@@ -163,22 +195,33 @@ function App() {
       });
     });
 
+    onSnapshot(callDocRef, (snapshot) => {
+      if (!snapshot.exists()) {
+        navigate("/ended", { replace: true });
+      }
+    });
+
     setHangUpButtonDisabled(false);
   };
 
-  const hangupCall = () => {
-    pc.close();
-    pc = new RTCPeerConnection(servers);
-    setCallInput("");
-    setAnswerCallButtonDisabled(true);
-    setHangUpButtonDisabled(true);
+  const hangupCall = async () => {
+    const callDocRef = doc(firestore, "calls", callID.current);
+    const offerCandidates = collection(callDocRef, "offerCandidates");
+    const answerCandidates = collection(callDocRef, "answerCandidates");
+    for (const candidate of (await getDocs(offerCandidates)).docs) {
+      deleteDoc(candidate.ref);
+    }
+    for (const candidate of (await getDocs(answerCandidates)).docs) {
+      deleteDoc(candidate.ref);
+    }
 
-    remoteVideo.current.srcObject = null;
+    deleteDoc(callDocRef);
+    navigate("/ended");
   };
 
   return (
-    <main className="text-center flex flex-col items-center justify-center gap-4 bg-slate-50 text-slate-800 w-full h-screen p-8">
-      <section className="w-full h-full min-h-0 bg-slate-500 rounded-lg relative overflow-hidden border-2 border-slate-400">
+    <>
+      <section className="w-full h-full min-h-0 bg-white0 rounded-lg relative overflow-hidden border-2 border-slate-400">
         <span className="w-64 aspect-video absolute bottom-4 right-4 border-2 border-slate-400 rounded-md overflow-hidden z-50">
           <Icon
             icon="ph:video-camera-slash-bold"
@@ -204,50 +247,167 @@ function App() {
             className="w-full h-full object-cover"
           />
         </span>
+        <span className="z-50 absolute bottom-4 left-4 bg-white pl-4 pr-2 py-3 rounded-md flex items-center gap-2 border-2 border-slate-400">
+          {_callID || "No call ID"}
+          <button
+            className="text-slate-500 px-2 py-1 rounded-md"
+            onClick={() => {
+              navigator.clipboard.writeText(_callID);
+              setLinkCopied(true);
+              setTimeout(() => {
+                setLinkCopied(false);
+              }, 2000);
+            }}
+          >
+            <Icon
+              icon={linkCopied ? "ph:check-bold" : "ph:copy-bold"}
+              className="w-5 h-5"
+            />
+          </button>
+        </span>
       </section>
 
-      <section className="flex gap-3">
-        <button
-          onClick={startWebCam}
-          className={`bg-rose-500 text-slate-50 w-16 h-16 flex items-center justify-center rounded-full`}
-        >
-          <Icon
-            icon={
-              isWebCamOn ? "ph:video-camera-bold" : "ph:video-camera-slash-bold"
-            }
-            className="w-5 h-5 text-slate-50"
-          />
-        </button>
-        <button
-          onClick={createCall}
-          disabled={createCallButtonDisabled}
-          className="bg-slate-600 text-slate-50 px-6 gap-2 h-16 rounded-full flex items-center justify-center disabled:bg-slate-400"
-        >
-          <Icon icon="ph:plus-bold" className="w-5 h-5" />
-          Create Call
-        </button>
-        <input
-          value={callInput}
-          onChange={(e) => setCallInput(e.target.value)}
-          className=" border-2 h-16 px-6 rounded-full border-slate-400 bg-transparent placeholder-slate-400 focus:ring-slate-300"
-          placeholder="Paste the call ID here"
-        />
-        <button
-          onClick={answerCall}
-          disabled={answerCallButtonDisabled}
-          className="bg-emerald-500 text-slate-50 w-16 h-16 rounded-full flex items-center justify-center disabled:bg-emerald-600"
-        >
-          <Icon icon="ph:arrow-right-bold" className="w-5 h-5" />
-        </button>
+      <section className="flex gap-3 flex-col sm:flex-row w-full items-center justify-center">
+        {!webCamButtonDisabled && (
+          <button
+            onClick={startWebCam}
+            className="bg-slate-600 text-slate-50 shadow-md w-full sm:w-auto px-6 gap-2 h-16 flex items-center justify-center rounded-full font-semibold tracking-widest text-sm"
+          >
+            <Icon
+              icon={
+                isWebCamOn
+                  ? "ph:video-camera-bold"
+                  : "ph:video-camera-slash-bold"
+              }
+              className="w-5 h-5 text-slate-50"
+            />
+            Turn on WebCam
+          </button>
+        )}
         {!hangUpButtonDisabled && (
           <button
             onClick={hangupCall}
-            className="bg-rose-500 text-slate-50 w-16 h-16 rounded-full disabled:bg-rose-700 flex items-center justify-center"
+            className="bg-rose-500 text-slate-50 w-full sm:w-16 h-16 rounded-full disabled:bg-rose-700 flex items-center justify-center"
           >
             <Icon icon="ph:phone-x-bold" className="w-5 h-5" />
           </button>
         )}
       </section>
+    </>
+  );
+}
+
+function CallEnded() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-8">
+      <h1 className="text-4xl text-slate-700">
+        The call has ended. Thanks for using!
+      </h1>
+      <Link to="/">
+        <button className="bg-emerald-500 w-full text-slate-50 px-12 py-4 rounded-full font-semibold tracking-widest shadow-md text-center">
+          Go Home
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+function CallNotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-8">
+      <h1 className="text-4xl text-slate-700">
+        The call you are looking for does not exist.
+      </h1>
+      <Link to="/">
+        <button className="bg-emerald-500 w-full text-slate-50 px-12 py-4 rounded-full font-semibold tracking-widest shadow-md text-center">
+          Go Home
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+function Home() {
+  const [callID, setCallID] = useState("");
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4 px-8 sm:px-16 md:px-32 lg:px-64 xl:px-96 text-slate-700">
+      <Icon icon="logos:webrtc" className="w-32 h-32 mb-4" />
+      <h1 className="text-4xl font-bold text-center">
+        Have fun with <span className="text-emerald-500">WebRTC</span>!
+      </h1>
+      <p className="text-center">
+        This is a simple video call app built with WebRTC technology. You can
+        create a call or join an existing call.
+      </p>
+      <div className="w-full flex items-center gap-2 mt-5">
+        <div
+          tabIndex={0}
+          className="w-full border-2 border-slate-200 focus-within:border-emerald-500 transition-all rounded-md overflow-hidden flex items-center gap-2 px-4"
+        >
+          <Icon
+            icon="ph:keyboard-bold"
+            className="w-5 h-5 text-slate-400 mb-0.5"
+          />
+          <input
+            className="w-full py-4 placeholder-slate-400 focus:outline-none text-slate-500 bg-transparent"
+            placeholder="Enter an existing call ID"
+            value={callID}
+            onChange={(e) => setCallID(e.target.value)}
+          />
+        </div>
+        <Link
+          to={`/call/${callID}`}
+          state={{ isNew: false }}
+          className="h-full"
+        >
+          <button
+            disabled={callID === ""}
+            className="bg-white h-full block text-emerald-500 transition-all disabled:text-slate-400 hover:bg-emerald-50 disabled:hover:bg-white rounded-md px-6 tracking-widest font-medium"
+          >
+            Join
+          </button>
+        </Link>
+      </div>
+      <div className="w-full border-b-2 border-slate-200 relative my-4">
+        <span className="text-slate-300 absolute bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-2 font-medium">
+          OR
+        </span>
+      </div>
+      <Link
+        to={`/call/${v4()}`}
+        state={{
+          isNew: true,
+        }}
+        className="bg-emerald-500 hover:bg-emerald-600 transition-all w-full text-slate-50 px-12 py-4 rounded-full font-semibold tracking-widest shadow-md text-center"
+      >
+        Create a Call
+      </Link>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <main className="text-center flex flex-col items-center justify-center gap-4 bg-white text-slate-700 w-full h-screen p-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/call/:id" element={<CallScreen />} />
+        <Route path="/ended" element={<CallEnded />} />
+        <Route path="/call-not-exist" element={<CallNotFound />} />
+      </Routes>
+      <p className="text-sm text-slate-500">
+        Made with ❤️ by{" "}
+        <a
+          href="https://thecodeblog.net"
+          target="_blank"
+          rel="noreferrer"
+          className="text-emerald-500 font-medium"
+        >
+          Melvin Chia
+        </a>
+        . Project under MIT License.
+      </p>
     </main>
   );
 }
